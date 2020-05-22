6f15aa79dea3751ef92af8a1f4d6165ac28ba6be
DeependhuRajan <deependhurjn@gmail.com>
2020-05-22 14:10:48 -0700

    New code added

---
+++ src/myJavaProject/myAppTester.java
@@ -0,0 +1,22 @@
+package myJavaProject;
+import java.util.Scanner;
+
+
+public class myAppTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int n = 0;
+		int s;
+		
+		System.out.println("Hello Software Engineering world");
+		System.out.println("Enter your number");
+		n = input.nextInt();
+		s=n*n;
+		System.out.println("square of the input number is : "+ s);
+		
+	}
+	
+
+}
